ba50c247f05f0675563f0c715eba5ce3c5e88ed5
Ron Cagle <ron@roncagle.com>
2021-05-19 22:10:56 -0400

    Code so far up to Stopwatch

--- JS_Module_3/06-Advanced-Dom-Manipulations/06. JS-Advanced-DOM-Manipulations-Lab-Resources/01. List-Of-Items/app.js
+++ JS_Module_3/06-Advanced-Dom-Manipulations/06. JS-Advanced-DOM-Manipulations-Lab-Resources/01. List-Of-Items/app.js
@@ -1,3 +1,21 @@
+console.log("its on");
 function addItem() {
-  console.log('TODO:...')
+	let toDoList = document.getElementById("items");
+
+	let newLi = document.createElement("li");
+	let inputTODO = document.getElementById("newItemText").value;
+	newLi.innerText = inputTODO;
+	toDoList.appendChild(newLi);
 }
+
+let submitButton = document.getElementById("submitButton");
+console.log(submitButton);
+submitButton.addEventListener("keyup", function (event) {
+	// Number 13 is the "Enter" key on the keyboard
+	if (event.keyCode === 13) {
+		// Cancel the default action, if needed
+		event.preventDefault();
+		// Trigger the button element with a click
+		document.getElementById("submitButton").click();
+	}
+});
